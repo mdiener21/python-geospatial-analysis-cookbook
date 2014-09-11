d027337eb9360bd736d708331848f1efbd5d641e
Michael Diener <mdiener21@users.noreply.github.com>
2014-09-11 20:23:57 +0200

    clean up code

--- ch03/code/ch03-01-call-ogr2ogr.py
+++ ch03/code/ch03-01-call-ogr2ogr.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import subprocess
 
 db_schema = "SCHEMA=geodata"
-pg_epsg = "900913"
+overwrite_option = "OVERWRITE=YES"
+geom_type = "MULTILINESTRING"
+output_format = "PostgreSQL"
 
 db_connection = "PG:host=localhost port=5432 \
 	user=postgres dbname=py_cb password=secret"
-
-output_format = "PostgreSQL"
-geom_type = "MULTILINESTRING"
+	
+pg_epsg = "900913"
 input_shp = "/home/mdiener/geodata/bikeways.shp"
-overwrite_option = "OVERWRITE=YES"
 
 subprocess.call(["ogr2ogr","-lco", db_schema, "-lco", overwrite_option, \
 	"-nlt", geom_type, "-f", output_format, db_connection,  input_shp])
