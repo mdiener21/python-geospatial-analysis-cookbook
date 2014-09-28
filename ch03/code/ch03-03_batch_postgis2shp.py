9b5d1b5162b87e3fbb0f4401e2b1087dbd26dba6
mdiener21 <mdiener21@gmail.com>
2014-09-28 11:18:27 +0200

    added ch03 3rd code recipe

---
+++ ch03/code/ch03-03_batch_postgis2shp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import subprocess
+import os
+
+destination_dir = "/home/mdiener/ch03/geodata/temp"
+postgis_tables_list = ["bikeways", "sportanl"]
+
+db_schema = "active_schema=geodata"
+db_connection = "PG:host=localhost port=5432 user=postgres \
+        dbname=py_cb password=air " +  db_schema
+output_format = "ESRI Shapefile"
+
+# check if destination directory exists
+if os.path.isdir(destination_dir):
+    for table in postgis_tables_list:
+        subprocess.call(["ogr2ogr", "-f", output_format, destination_dir,  \
+            db_connection, table])
+        print("running ogr2ogr on table: " + table)
+else:
+    print("oh no your destination directory" + destination_dir + "does not exist please create it then run again")
+
+# commandline call without using python will look like this
+#ogr2ogr -f "ESRI Shapefile" mydatadump \
+# PG:"host=myhost user=myloginname dbname=mydbname password=mypassword" neighborhood parcels
+
