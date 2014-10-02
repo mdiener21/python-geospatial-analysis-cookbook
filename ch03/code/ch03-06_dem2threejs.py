de8ccfb7bf9a7007c8e3460199f618fcbcdd6ed2
mdiener21 <mdiener21@gmail.com>
2014-10-03 00:38:41 +0200

    added last call tif to envi format

--- ch03/code/ch03-06_dem2threejs.py
+++ ch03/code/ch03-06_dem2threejs.py
@@ -28,6 +28,8 @@ new_clip_ktn_dem = path_base + "ktn_clip-dhm5.asc"
 
 final_heightmap = path_base + "final_heightmap.png"
 
+output_envi = path_base + "final_envi.bin"
+
 
 ####subprocess.call(["gdalbuildvrt", temp_vrt, orig_dem_asc])
 # gdalbuildvrt jotunheimen.vrt my_elevation.dem
@@ -50,7 +52,6 @@ dem2tiff = command_gdalwarp + " " + new_clip_ktn_dem + " "  + temp_tiff
 print ("now executing this command: " + dem2tiff)
 #subprocess.call(dem2tiff.split(), shell=False)
 
-
 show_tif_info = command_gdalinfo + " -mm " + temp_tiff
 tif_info = subprocess.call(show_tif_info.split(), shell=False)
 print type(tif_info)
@@ -66,39 +67,8 @@ else:
 
 translate2png = command_gdal_translate + " -scale 700 851 0 255 -outsize 200 200 -of PNG " + temp_tiff + " " + final_heightmap
 
-#subprocess.call(translate2png.split(), shell=False)
-#gdal_translate -scale 0 2470 0 255 -outsize 200 200 -of PNG jotunheimen.tif jotunheimen.png
-
-# gdal_translate -scale 0 2470 0 65535 -ot UInt16 -outsize 200 200 -of ENVI jotunheimen.tif jotunheimen.bin
-#gdal_translate -projwin 478036.238528 185166.392009 478494.852057 184768.350079 C:/00_GOMOGI/geodata/ALS_DGM_10m.asc
-#gdal_translate -projwin 478036.238528 185166.392009 478494.852057 184768.350079 -of AAIGrid C:/00_GOMOGI/geodata/ALS_DGM_10m.asc C:/Users/Michael/Downloads/ktn_clip-dhm.asc
-
-# clip dem and convert to .tif
-# if you want to clip to extents use -te [-te xmin ymin xmax ymax]
-# 1. gdalwarp to clip and convert to tif
-####subprocess.call(['gdalwarp',"-te", "","","","", temp_vrt, temp_tiff])
-
-#gdalwarp jotunheimen.vrt jotunheimen.tif
-#http://www.gdal.org/gdalwarp.html
-
-####print (subprocess.call(["gdalinfo", "-mm", temp_tiff]))
-
-# show new tiff image information
-# gdalinfo -mm jotunheimen.tif
-
 ####subprocess.call(["gdal_translate", "-ot", "UInt16", "-outsize", "200", "200", "-of", "ENVI", temp_tiff, threejs_webgl_dem])
+subprocess.call(['gdalwarp',"-te", "","","","", temp_vrt, temp_tiff])
 
+tif_2_envi = "gdal_translate -scale 700 851 0 65535 -ot UInt16 -outsize 200 200 -of ENVI " + temp_tiff + " " + output_envi
 #gdal_translate -scale 0 2470 0 65535 -ot UInt16 -outsize 200 200 -of ENVI jotunheimen.tif jotunheimen.bin
-# http://www.gdal.org/gdal_translate.html
-
-# gdalwarp -te 484500 6818000 486500 6820000 jotunheimen.vrt besseggen.tif
-#
-# gdal_translate -scale 982 1742 0 255 -of PNG besseggen.tif besseggen.png
-#
-# gdal_translate -scale 982 1905 0 65535 -ot UInt16 -of ENVI besseggen.tif besseggen.bin
-#
-# Lastly, I'm creating a tiny heightmap of only 10 x 10 px for testing purposes:
-# -of is output format here .png is output
-# gdal_translate -scale 982 1905 0 255 -outsize 10 10 -of PNG besseggen.tif besseggen10.png
-#-of is ENVI format .bin for threejs
-# gdal_translate -scale 982 1905 0 65535 -outsize 10 10 -ot UInt16 -of ENVI besseggen.tif besseggen10.bin
