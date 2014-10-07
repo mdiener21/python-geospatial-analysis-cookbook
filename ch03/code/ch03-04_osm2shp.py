5d05ad199314519eb537170461720a1c1feb4207
mdiener21 <mdiener21@gmail.com>
2014-10-07 21:35:31 +0200

    recipe ch03-04 cleaned up and added comments

--- ch03/code/ch03-04_osm2shp.py
+++ ch03/code/ch03-04_osm2shp.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 # convert / import osm xml .osm file into a Shapefile
 import subprocess
 
@@ -9,33 +8,17 @@ import subprocess
 output_format = "ESRI Shapefile"
 
 # complete path to input OSM xml file .osm
-input_osm = '../geodata/map.osm'
+input_osm = '../geodata/OSM_san_francisco_westbluff.osm'
 
+# path to a new folder to hold the Shapefiles (must not exist)
+output_shp_folder = '../geodata/shp'
 
-# complete path to input Shapefile
-output_shp = '../geodata/shp'
+# Windows users can uncomment these two lines if needed
+# ogr2ogr = r"c:/OSGeo4W/bin/ogr2ogr.exe"
+# ogr_info = r"c:/OSGeo4W/bin/ogrinfo.exe"
 
-ogr2ogr = r"c:/OSGeo4W/bin/ogr2ogr.exe"
-ogr_info = r"c:/OSGeo4W/bin/ogrinfo.exe"
-
-#option_point = "SHPT=POINT"
-option_line = "SHPT=ARC"
-# SHPT=POINT/ARC/POLYGON/MULTIPOINT/POINTZ/ARCZ/POLYGONZ/MULTIPOINTZ.
-#multipolygons = "multipolygons"
-
+# view what geometry types are available in our OSM file
 subprocess.call([ogr_info, input_osm])
 
 # call the subprocess function which fires ogr2ogr command line
-#subprocess.call([ogr2ogr, "-lco", option_line, "-f", output_format, output_shp, input_osm])
-
-subprocess.call([ogr2ogr, "-f", output_format, output_shp, input_osm, 'waypoints'])
-#subprocess.call([ogr2ogr, output_shp, multipolygons])
-#ogr2ogr cite_soleil_buildings cite_soleil_buildings.osm multipolygons
-
-###
-#\ch03\code>c:\OSGeo4W\bin\ogr2ogr.exe ..\geodata\4foo ..\geodata\map.osm multipolygons
-# 1: points (Point)
-# 2: lines (Line String)
-# 3: multilinestrings (Multi Line String)
-# 4: multipolygons (Multi Polygon)
-# 5: other_relations (Geometry Collection)
+subprocess.call([ogr2ogr, "-f", output_format, output_shp_folder, input_osm])
