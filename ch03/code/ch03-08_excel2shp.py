51522f0fcfe339ad678fa8c7f8bf68ac93d13b8f
mdiener21 <mdiener21@gmail.com>
2014-10-11 23:51:36 +0200

    added print out message

--- ch03/code/ch03-08_excel2shp.py
+++ ch03/code/ch03-08_excel2shp.py
@@ -5,10 +5,10 @@ import xlrd
 import shapefile
 
 # open the excel file
-book = xlrd.open_workbook("../geodata/highest-mountains-europe.xlsx")
+excel_file = xlrd.open_workbook("../geodata/highest-mountains-europe.xlsx")
 
 # get the first sheet
-sh = book.sheet_by_index(0)
+sh = excel_file.sheet_by_index(0)
 w = shapefile.Writer(shapefile.POINT)
 
 # fields available GeoNameId	Name	Country	Latitude	Longitude	Altitude (m)
@@ -19,16 +19,19 @@ w.field('Latitude', 'F')
 w.field('Longitude', 'F')
 w.field('Altitude', 'F')
 
-
+# loop over each row in the excel sheet
 for rownum in range(sh.nrows):
-    x_coord = sh.cell_value(rowx=rownum, colx=4)
-    y_coord = sh.cell_value(rowx=rownum, colx=3)
-    w.point(x_coord, y_coord)
-    print x_coord
-    print y_coord
+    # skips over the first row since it is the header row
+    if rownum == 0:
+        continue
+    else:
+        x_coord = sh.cell_value(rowx=rownum, colx=4)
+        y_coord = sh.cell_value(rowx=rownum, colx=3)
+        w.point(x_coord, y_coord)
 
-    w.record(GeoNameId=sh.cell_value(rowx=rownum, colx=0), Name=sh.cell_value(rowx=rownum, colx=1),
-             Country=sh.cell_value(rowx=rownum, colx=2), Latitude=sh.cell_value(rowx=rownum, colx=3),
-             Longitude=sh.cell_value(rowx=rownum, colx=4),Altitude=sh.cell_value(rowx=rownum, colx=5))
+        w.record(GeoNameId=sh.cell_value(rowx=rownum, colx=0), Name=sh.cell_value(rowx=rownum, colx=1),
+                 Country=sh.cell_value(rowx=rownum, colx=2), Latitude=sh.cell_value(rowx=rownum, colx=3),
+                 Longitude=sh.cell_value(rowx=rownum, colx=4),Altitude=sh.cell_value(rowx=rownum, colx=5))
+        print "Adding row: " + str(rownum) + " creating mount: " + sh.cell_value(rowx=rownum, colx=1)
 
-w.save('highest-mountains')
+w.save('../geodata/highest-mountains')
