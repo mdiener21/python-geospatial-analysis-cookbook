18272e5b2fcb460b90af404f261361c4b65dc61b
mdiener21 <mdiener21@gmail.com>
2014-10-13 20:25:33 +0200

    pep 8 fixes path update

--- ch03/code/ch03-02_batch_shp2pg.py
+++ ch03/code/ch03-02_batch_shp2pg.py
@@ -5,24 +5,23 @@ import subprocess
 import os
 
 def run_shp2pg(input_shp):
-	"""
-	input_shp is full path to shapefile including file ending
-	usage:  run_shp2pg('/home/geodata/myshape.shp')
-	"""
+    """
+    input_shp is full path to shapefile including file ending
+    usage:  run_shp2pg('/home/geodata/myshape.shp')
+    """
 
-	db_schema = "SCHEMA=geodata"
-	db_connection = "PG:host=localhost port=5432 \
-
-		user=postgres dbname=py_geoan_cb password=air"
-	output_format = "PostgreSQL"
-	geom_type = "MULTILINESTRING"
-	#input_shp = "/home/mdiener/geodata/bikeways.shp"
-	overwrite_option = "OVERWRITE=YES"
-	subprocess.call(["ogr2ogr","-lco", db_schema, "-lco", overwrite_option, \
-		"-nlt", geom_type, "-f", output_format, db_connection,  input_shp])
+    db_schema = "SCHEMA=geodata"
+    db_connection = "PG:host=localhost port=5432 \
+    user=postgres dbname=py_geoan_cb password=air"
+    output_format = "PostgreSQL"
+    geom_type = "MULTILINESTRING"
+    # input_shp = "../geodata/bikeways.shp"
+    overwrite_option = "OVERWRITE=YES"
+    subprocess.call(["ogr2ogr", "-lco", db_schema, "-lco", overwrite_option,
+    "-nlt", geom_type, "-f", output_format, db_connection,  input_shp])
 
 # directory full of shapefiles
-shapefile_dir = "/home/mdiener/geodata"
+shapefile_dir = "../geodata"
 
 # empty list to hold names of all shapefils in directory
 shapefile_list = []
@@ -35,5 +34,5 @@ for file in os.listdir(shapefile_dir):
 
 # loop over list of Shapefiles running our import function
 for each_shapefile in shapefile_list:
-	run_shp2pg(each_shapefile)
-	print ("importing Shapefile: " + each_shapefile)
+    run_shp2pg(each_shapefile)
+    print ("importing Shapefile: " + each_shapefile)
