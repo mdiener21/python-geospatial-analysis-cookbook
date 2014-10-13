61b19f5700a59927073c54be68cf6680f77cef47
mdiener21 <mdiener21@gmail.com>
2014-10-13 21:05:10 +0200

    pep 8 reformat

--- ch03/code/ch03-03_batch_postgis2shp.py
+++ ch03/code/ch03-03_batch_postgis2shp.py
@@ -5,7 +5,7 @@ import subprocess
 import os
 
 # folder to hold output Shapefiles
-destination_dir = "/home/mdiener/ch03/geodata/temp"
+destination_dir = os.path.dirname(os.path.realpath('..\geodata')) + "\\geodata\\temp"
 
 # list of postGIS tables
 postgis_tables_list = ["bikeways", "sportanl"]
@@ -13,20 +13,20 @@ postgis_tables_list = ["bikeways", "sportanl"]
 # database connection parameters
 db_schema = "active_schema=geodata"
 db_connection = "PG:host=localhost port=5432 user=postgres \
-        dbname=py_geoan_cb password=air " +  db_schema
+        dbname=py_geoan_cb password=air " + db_schema
 
 output_format = "ESRI Shapefile"
 
 # check if destination directory exists
 if os.path.isdir(destination_dir):
     for table in postgis_tables_list:
-        subprocess.call(["ogr2ogr", "-f", output_format, destination_dir,  \
-            db_connection, table])
+        subprocess.call(["ogr2ogr", "-f", output_format, destination_dir,
+                         db_connection, table])
         print("running ogr2ogr on table: " + table)
 else:
     print("oh no your destination directory" + destination_dir + "does not exist please create it then run again")
 
 # commandline call without using python will look like this
-#ogr2ogr -f "ESRI Shapefile" mydatadump \
+# ogr2ogr -f "ESRI Shapefile" mydatadump \
 # PG:"host=myhost user=myloginname dbname=mydbname password=mypassword" neighborhood parcels
 
