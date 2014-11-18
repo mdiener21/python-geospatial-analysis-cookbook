2e3bf71b7282a33b94f1d02d3075d553d8bb00f6
Michael Diener <mdiener21@gmail.com>
2014-11-18 19:48:02 +0100

    update path and shapefile name

--- ch02/code/ch02-01_show_shp_crs.py
+++ ch02/code/ch02-01_show_shp_crs.py
@@ -1,6 +1,6 @@
 from osgeo import ogr
 shp_driver = ogr.GetDriverByName('ESRI Shapefile')
-shp_dataset = shp_driver.Open(r'../../geodata/SPORTANL.shp')
+shp_dataset = shp_driver.Open(r'../geodata/schools.shp')
 shp_layer = shp_dataset.GetLayer()
 shp_srs = shp_layer.GetSpatialRef()
 print shp_srs
