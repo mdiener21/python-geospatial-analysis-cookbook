74fa6f74c5531a886f963b04d7644d1e60472584
Michael Diener <mdiener21@gmail.com>
2014-11-20 23:59:37 +0100

    update to match code

--- ch04/code/sql_postgis.sql
+++ ch04/code/sql_postgis.sql
@@ -1,12 +1,12 @@
-select ST_AsGeoJSON(ST_Buffer(st_transform(wkb_geometry, 3857), 100)) as geom, name, country,
-               altitude from geodata.highest_mountains where ogc_fid = 2;
+SELECT ST_AsGeoJSON(ST_Transform(ST_Buffer(wkb_geometry, 100,'quad_segs=8'),4326)) AS geom, name
+               FROM geodata.schools;
 
 SELECT Find_SRID('geodata', 'highest_mountains', 'wkb_geometry');
 
-Select ST_SetSRID(wkb_geometry, 4326) from geodata.highest_mountains;
+SELECT ST_SetSRID(wkb_geometry, 4326) FROM geodata.highest_mountains;
 
 SELECT UpdateGeometrySRID('geodata.highest_mountains','wkb_geometry',4326);
 
-select UpdateGeometrySRID('geodata', 'highest_mountains', 'wkb_geometry', 4326);
+SELECT UpdateGeometrySRID('geodata', 'highest_mountains', 'wkb_geometry', 4326);
 
-select UpdateGeometrySRID('geodata', 'bike_shops_3785', 'wkb_geometry', 3857);
+SELECT UpdateGeometrySRID('geodata', 'bike_shops_3785', 'wkb_geometry', 3857);
