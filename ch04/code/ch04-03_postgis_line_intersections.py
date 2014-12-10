c91d02174f330a98b424a4d17072008e303adc6b
mdiener21 <mdiener21@gmail.com>
2014-12-10 23:28:05 +0100

    restructure query for easier readability

--- ch04/code/ch04-03_postgis_line_intersections.py
+++ ch04/code/ch04-03_postgis_line_intersections.py
@@ -21,7 +21,17 @@ cur = conn.cursor()
 
 
 split_lines_query = """
-create table geodata.temp_roads as select (st_dump(st_node(st_collect(geom)))).geom as geom from geodata.van_raw_roads;
-ALTER TABLE geodata.temp_roads ADD COLUMN id serial;
-ALTER TABLE geodata.temp_roads ADD CONSTRAINT temp_pkey PRIMARY KEY (id);
-"""
+                    CREATE TABLE
+                        geodata.split_roads
+                    AS SELECT
+                        (ST_Dump(ST_Node(ST_Collect(geom)))).geom AS GEOM
+                    FROM
+                        geodata.van_raw_roads;
+                     """
+cur.execute("ALTER TABLE geodata.temp_roads ADD COLUMN id serial;")
+cur.execute("ALTER TABLE geodata.temp_roads ADD CONSTRAINT temp_pkey PRIMARY KEY (id);")
+
+# create table geodata.temp_roads as select (st_dump(st_node(st_collect(geom)))).geom as geom from geodata.van_raw_roads;
+
+
+
