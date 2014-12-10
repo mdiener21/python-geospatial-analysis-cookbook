289e57040402a51eaa7ec97743a98d8bd61e939c
mdiener21 <mdiener21@gmail.com>
2014-12-10 23:23:28 +0100

    added split at intersections st_node query

--- ch04/code/ch04-03_postgis_line_intersections.py
+++ ch04/code/ch04-03_postgis_line_intersections.py
@@ -19,40 +19,9 @@ conn = psycopg2.connect(host=db_host, user=db_user, port=db_port, password=db_pa
 # create a cursor
 cur = conn.cursor()
 
-# generate points where lines intesect
 
-intersection_query = """
-                        CREATE TABLE network_intersections as
-                        SELECT
-                            ST_Intersection(a.geom, b.geom),
-                            Count(Distinct a.gid)
-                        FROM
-                            geodata.roads as a,
-                            geodata.roads as b
-                        WHERE
-                            ST_Touches(a.geom, b.geom)
-                            AND a.gid != b.gid
-                        GROUP BY
-                            ST_Intersection(a.geom, b.geom)
-                        ;
-                    """
-
-# postgis function to create a fishnet grid
-# http://gis.stackexchange.com/questions/16374/how-to-create-a-regular-polygon-grid-in-postgis
-create_function_query = """
-CREATE OR REPLACE FUNCTION ST_CreateFishnet(
-        nrow integer, ncol integer,
-        xsize float8, ysize float8,
-        x0 float8 DEFAULT 0, y0 float8 DEFAULT 0,
-        OUT "row" integer, OUT col integer,
-        OUT geom geometry)
-    RETURNS SETOF record AS
-$$
-SELECT i + 1 AS row, j + 1 AS col, ST_Translate(cell, j * $3 + $5, i * $4 + $6) AS geom
-FROM generate_series(0, $1 - 1) AS i,
-     generate_series(0, $2 - 1) AS j,
-(
-SELECT ('POLYGON((0 0, 0 '||$4||', '||$3||' '||$4||', '||$3||' 0,0 0))')::geometry AS cell
-) AS foo;
-$$ LANGUAGE sql IMMUTABLE STRICT;
+split_lines_query = """
+create table geodata.temp_roads as select (st_dump(st_node(st_collect(geom)))).geom as geom from geodata.van_raw_roads;
+ALTER TABLE geodata.temp_roads ADD COLUMN id serial;
+ALTER TABLE geodata.temp_roads ADD CONSTRAINT temp_pkey PRIMARY KEY (id);
 """
