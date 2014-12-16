3e0859642d2d9357c2da0b7ffab16cb4bb1a6a94
mdiener21 <mdiener21@gmail.com>
2014-12-16 10:18:27 +0100

    renamed file and updated code and query

---
+++ ch04/code/ch04-04_postgis_join_pt_attributes2ply.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import psycopg2
+
+# Database Connection Info
+db_host = "localhost"
+db_user = "postgres"
+db_passwd = "air"
+db_database = "py_geoan_cb"
+db_port = "5432"
+
+# connect to DB
+conn = psycopg2.connect(host=db_host, user=db_user, port=db_port, password=db_passwd, database=db_database)
+
+# create a cursor
+cur = conn.cursor()
+
+
+# assign polygon attributes from points
+spatial_join = """  UPDATE geodata.pebble_beach_greens AS g
+                        SET
+                           name = h.name
+                        FROM
+                           geodata.pebble_beach_hole_num AS h
+                        WHERE
+                           ST_Contains(g.geom, h.geom);
+                     """
+cur.execute(spatial_join)
+conn.commit()
+
+# close cursor
+cur.close()
+
+# close connection
+conn.close()
