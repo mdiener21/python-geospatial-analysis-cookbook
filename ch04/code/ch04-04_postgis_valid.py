1d4434f8d1ff5ced74d6660c52dff17d5225a84d
mdiener21 <mdiener21@gmail.com>
2014-12-17 23:57:43 +0100

    added code for ch04-04 postgis validity

---
+++ ch04/code/ch04-04_postgis_valid.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import psycopg2
+
+# Database Connection Info
+db_host = "localhost"
+db_user = "postgres"
+db_passwd = "air"
+db_database = "py_geoan_cb"
+db_port = "5432"
+
+# connect to DB
+conn = psycopg2.connect(host=db_host, user=db_user, port=db_port, password=db_passwd, database=db_database)
+
+# create a cursor
+cur = conn.cursor()
+
+# the PostGIS buffer query
+valid_query = """SELECT
+                   ogc_fid, 
+                   ST_IsValidDetail(wkb_geometry)
+                FROM 
+                   geodata.lines
+                WHERE NOT
+                   ST_IsValid(wkb_geometry);
+                """
+
+# execute the query
+cur.execute(valid_query)
+
+# return all the rows, we expect more than one
+validity_results = cur.fetchall()
+
+print validity_results
+
+# close cursor
+cur.close()
+
+# close connection
+conn.close()
