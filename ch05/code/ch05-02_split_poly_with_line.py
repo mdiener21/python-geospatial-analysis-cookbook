2d08017cde04076124f3107fd0c0e50dd30604dd
mdiener21 <mdiener21@gmail.com>
2015-01-21 23:04:57 +0100

    ch05-02 split polgon with linestring first commit

---
+++ ch05/code/ch05-02_split_poly_with_line.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from math import sqrt
+
+import shapefile
+from matplotlib import pyplot
+from descartes import PolygonPatch
+import shapely
+from shapely.geometry import Polygon, LineString, Point
+from shapely.ops import polygonize
+# used to import dictionary data to shapely
+from shapely.geometry import asShape
+from shapely.geometry import mapping
+
+# calculate the size of our matplotlib output
+GM = (sqrt(5) - 1.0) / 2.0
+W = 8.0
+H = W * GM
+SIZE = (W, H)
+
+# colors for our plots as hex
+GRAY = '#00b700'
+BLUE = '#6699cc'
+YELLOW = '#ffe680'
+
+
+# functions slightly modified from Sean Gilles http://toblerity.org/shapely/
+# used for drawing our results using matplotlib
+
+def plot_coords_line(axis, object, color='#00b700'):
+    x, y = object.xy
+    ax.plot(x, y, 'o', color=color, zorder=1)
+
+
+def plot_coords_lines(axis, object, color='#999999'):
+    for linestring in object:
+        x, y = linestring.xy
+        ax.plot(x, y, 'o', color=color, zorder=2)
+
+
+def plot_line(axis, object, color='#00b700'):
+    x, y = object.xy
+    ax.plot(x, y, color=color, linewidth=3, zorder=1)
+
+
+def plot_lines(axis, object, color='#00b700'):
+    for line in object:
+        x, y = line.xy
+        ax.plot(x, y, color=color, alpha=0.4, linewidth=1, solid_capstyle='round', zorder=2)
+
+
+def set_plot_bounds(object, offset=1.0):
+    """
+    Creates the limits for x and y axis plot
+
+    :param object: input shapely geometry
+    :param offset: amount of space around edge of features
+    :return: dictionary of x-range and y-range values for
+    """
+    bounds = object.bounds
+    x_min = bounds[0]
+    y_min = bounds[1]
+    x_max = bounds[2]
+    y_max = bounds[3]
+    x_range = [x_min - offset, x_max + offset]
+    y_range = [y_min - offset, y_max + offset]
+
+    return {'xrange': x_range, 'yrange': y_range}
+
+roads_input_shp = r"../geodata/roads_london_3857.shp"
+
+def shape_to_shply(shp_path):
+    shape_reader = shapefile.Reader(shp_path)
+    features = shape_reader.shapeRecords()
+
+    for feature in features:
+        shply_obj = asShape(feature.shape.__geo_interface__)
+        print shply_obj
+
+    print shply_obj
+
+    #return shply_obj
+
+
+shape_to_shply(roads_input_shp)
+
+def clip_feature(lines_to_clip, clip_area):
+
+    roads_features = roads_london.shapeRecords()
+    pass
+
+def shply_to_shape(object):
+    pass
+
+# # open roads Shapefile that we want to clip with pyshp
+# roads_london = shapefile.Reader(r"../geodata/roads_london_3857.shp")
+#
+# # open circle polygon with pyshp
+# clip_area = shapefile.Reader(r"../geodata/clip_area_3857.shp")
+#
+# # access the geometry of the clip area circle
+# clip_feature = clip_area.shape()
+#
+# # convert pyshp object to shapely
+# clip_shply = asShape(clip_feature)
+#
+# # create a list of all roads features and attributes
+# roads_features = roads_london.shapeRecords()
+#
+# # variables to hold new geometry
+# roads_clip_list = []
+# roads_shply = []
+#
+# # run through each geometry, convert to shapely geom and intersect
+# for feature in roads_features:
+#     roads_london_shply = asShape(feature.shape.__geo_interface__)
+#     roads_shply.append(roads_london_shply)
+#     roads_intersect = roads_london_shply.intersection(clip_shply)
+#
+#     # only export linestrings, shapely also created points
+#     if roads_intersect.geom_type == "LineString":
+#         roads_clip_list.append(roads_intersect)
+#
+# # open writer to write our new shapefile too
+# pyshp_writer = shapefile.Writer()
+#
+# # create new field
+# pyshp_writer.field("name")
+#
+# # convert our shapely geometry back to pyshp, record for record
+# for feature in roads_clip_list:
+#     geojson = mapping(feature)
+#
+#     # create empty pyshp shape
+#     record = shapefile._Shape()
+#
+#     # shapeType 3 is linestring
+#     record.shapeType = 3
+#     record.points = geojson["coordinates"]
+#     record.parts = [0]
+#
+#     pyshp_writer._shapes.append(record)
+#     # add a list of attributes to go along with the shape
+#     pyshp_writer.record(["empty record"])
+#
+# # save to disk
+# pyshp_writer.save(r"../geodata/roads_clipped.shp")
+
+# setup matplotlib figure that will display the results
+fig = pyplot.figure(1, figsize=SIZE, dpi=90, facecolor="white")
+
+# add a little more space around subplots
+fig.subplots_adjust(hspace=.5)
+
+# ###################################
+#             first plot
+#  display sample line and circle
+# ###################################
+
+# first figure upper left drawing
+# 222 represents the number_rows, num_cols, subplot number
+ax = fig.add_subplot(121)
+
+# our demonstration geometries to see the details
+line = LineString([(0, 1), (3, 1), (0, 0)])
+polygon = Polygon(Point(1.5, 1).buffer(1))
+
+# use of descartes to create polygon in matplotlib
+# input circle and color fill and outline in blue with transparancy
+patch1 = PolygonPatch(polygon, fc=BLUE, ec=BLUE, alpha=0.5, zorder=1)
+
+# add circle to axis in figure
+ax.add_patch(patch1)
+
+# add line using our function above
+plot_line(ax, line)
+
+# draw the line nodes using our function
+plot_coords_line(ax, line)
+
+# subplot title text
+ax.set_title('Input line and circle')
+
+# define axis ranges as list [x-min, x-max]
+# added 1.5 units around object so not touching the sides
+x_range = [polygon.bounds[0] - 1.5, polygon.bounds[2] + 1.5]
+
+# y-range [y-min, y-max]
+y_range = [polygon.bounds[1] - 1.0, polygon.bounds[3] + 1.0]
+
+# set the x and y axis limits
+ax.set_xlim(x_range)
+ax.set_ylim(y_range)
+
+# assing the aspect ratio
+ax.set_aspect(1)
+
+
+# ###################################
+#             second plot
+#  display sample intersection
+# ###################################
+
+ax = fig.add_subplot(122)
+
+# run the intersection detail view
+cirle_as_line = polygon.boundary
+result_union_lines = cirle_as_line.union(line)
+new_polygons = polygonize(result_union_lines)
+
+output = dict(type='FeatureCollection', features=[])
+
+# http://stackoverflow.com/questions/25374459/find-holes-in-a-union-of-rectangles
+for poly in new_polygons:
+    # check if new poly is inside original polygon otherwise ignore it
+    if poly.centroid.within(polygon):
+        hmm = poly.centroid
+        ax.plot(hmm.x, hmm.y, 'o', color='#999999')
+        print "finally"
+        patch3 = PolygonPatch(poly, fc='purple', alpha=0.5, zorder=2)
+        ax.add_patch(patch3)
+    else:
+        # draw centroid of new polygon NOT inside original polygon
+        hmm = poly.centroid
+        ax.plot(hmm.x, hmm.y, 'o', color='#FF1813')
+        print "NOPE not inside the original polygon"
+
+
+# write title of second plot
+ax.set_title('Line intersects circle')
+
+# define the area that plot will fit into
+x_range = set_plot_bounds(polygon, 1.5)['xrange']
+y_range = set_plot_bounds(polygon, 1)['yrange']
+
+ax.set_xlim(*x_range)
+ax.set_ylim(*y_range)
+ax.set_aspect(1)
+
+pyplot.show()
