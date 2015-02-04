ef793479a0e4a5e48275d458cd09642a3b06c84b
mdiener21 <mdiener21@gmail.com>
2015-02-04 20:16:44 +0100

    plot split polygon

--- ch05/code/ch05-02_split_poly_with_line.py
+++ ch05/code/ch05-02_split_poly_with_line.py
@@ -10,12 +10,8 @@ from utils import SIZE, BLUE
 from utils import plot_coords_line
 from utils import plot_line
 from utils import set_plot_bounds
-from utils import shply_to_shape
 
-
-roads_input_shp = r"../geodata/roads_london_3857.shp"
-clip_area = r"../geodata/clip_area_3857.shp"
-
+##########################
 
 # setup matplotlib figure that will display the results
 fig = pyplot.figure(1, figsize=SIZE, dpi=90, facecolor="white")
@@ -65,6 +61,58 @@ ax.set_ylim(y_range)
 
 # assing the aspect ratio
 ax.set_aspect(1)
+
+
+###################################
+#             second plot
+#  display sample intersection
+# ###################################
+
+ax = fig.add_subplot(122)
+
+# convert circle polygon to linestring of circle boundary
+cirle_as_line = polygon.boundary
+
+# combine new boundary lines with the input set of lines
+result_union_lines = cirle_as_line.union(line)
+
+# re-create polygons from unioned lines
+new_polygons = polygonize(result_union_lines)
+
+# stores the final split up polygons
+new_cut_ply = []
+
+# identify which new polygon we want to keep
+for poly in new_polygons:
+    # check if new poly is inside original otherwise ignore it
+    if poly.centroid.within(polygon):
+        # center_pt = poly.centroid
+        # ax.plot(center_pt.x, center_pt.y, 'o', color='#999999')
+        print "creating new split polygon"
+        patch3 = PolygonPatch(poly, fc='purple',
+                              alpha=0.5, zorder=2)
+        ax.add_patch(patch3)
+        # add only polygons that overlap original for export
+        new_cut_ply.append(poly)
+    else:
+        # draw centroid of new polygon NOT inside original polygon
+        # center_pt = poly.centroid
+        # ax.plot(center_pt.x, center_pt.y, 'o', color='#FF1813')
+        print "This polygon is outside of the input features"
+
+
+# write title of second plot
+ax.set_title('Line intersects circle')
+
+# define the area that plot will fit into
+x_range = set_plot_bounds(polygon, 1.5)['xrange']
+y_range = set_plot_bounds(polygon, 1)['yrange']
+
+ax.set_xlim(*x_range)
+ax.set_ylim(*y_range)
+ax.set_aspect(1)
+
+pyplot.show()
 
 
 # ###################################
@@ -117,9 +165,3 @@ ax.set_ylim(*y_range)
 ax.set_aspect(1)
 
 pyplot.show()
-
-# full path to where we will store the shapefile
-split_poly_output = r"../geodata/split_up_poly_circle.shp"
-
-# execute our conversion from Shapely to Shapefile
-shply_to_shape(new_cut_ply, split_poly_output)
