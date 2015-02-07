7dbbb9ef5b963dbcbd9a3f03b9c3a95746b7d5f9
mdiener21 <mdiener21@gmail.com>
2015-02-07 13:45:18 +0100

    update comment

--- ch05/code/ch05-03_dist_between_2plys.py
+++ ch05/code/ch05-03_dist_between_2plys.py
@@ -26,7 +26,7 @@ print point
 print "pt_snap_res coordinate"
 print pt_snap_res
 
-print "pt_interpolate"
+print "interpolted point location"
 print pt_interpolate
 
 print "distance from origin to snap point"
