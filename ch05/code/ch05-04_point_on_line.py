5d38b6e881350338573d930bdea2c8589767a238
mdiener21 <mdiener21@gmail.com>
2015-02-10 23:34:50 +0100

    clean up and simplify

--- ch05/code/ch05-04_point_on_line.py
+++ ch05/code/ch05-04_point_on_line.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from shapely.ops import snap
-from matplotlib import pyplot
 from shapely.geometry import LineString, Point
-from utils import SIZE, BLUE, RED, GREEN, GRAY
-from utils import plot_coords_line
-from utils import plot_line
-
-# import fiona
-#
-# # open our shapefile
-# with fiona.open('../geodata/someshape.shp', mode='r') as source_shp:
 
 line = LineString([(0.5, 0.5), (2.0, 1.5), (3.0, 0.5)])
-h = line.coords
-print h
-reverse_line = 'hello world'[::-1]
-print reverse_line
-point = Point(3, 1.8)
-pt_snap_res = snap(point, line, 4)
-#
-shplySnapPoint = line.interpolate(1.4)
-print shplySnapPoint
+
+line_original = list(line.coords)
+print line_original
+
+line_reversed = list(line.coords)[::-1]
+print line_reversed
+
+point_on_line = line.interpolate(1.4)
+print point_on_line
