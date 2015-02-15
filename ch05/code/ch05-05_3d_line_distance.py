221c00ed2069510fd618f7d42c58314dae733737
mdiener21 <mdiener21@gmail.com>
2015-02-15 22:01:32 +0100

    clean code pep8 refactor

--- ch05/code/ch05-05_3d_line_distance.py
+++ ch05/code/ch05-05_3d_line_distance.py
@@ -5,80 +5,89 @@ from shapely.geometry import shape, Point
 import json
 
 
-
 def pairs(lst):
+    """
+    yield iterator of two coordinates of linestring
+    :param lst: list object
+    :return: yield iterator of two coordinates
+    """
     for i in range(1, len(lst)):
         yield lst[i - 1], lst[i]
 
 
-def calc_3d_distance_2pts(x1, y1, x2, y2, z1, z2):
+def calc_3d_distance_2pts(x1, y1, z1, x2, y2, z2):
+    """
+    :input two point coordinates (x1,y1,z1),(x2,y2,2)
+    :param x1: x coordinate first segment
+    :param y1: y coordiante first segment
+    :param z1: z height value first coordinate
+    :param x2: x coordinate second segment
+    :param y2: y coordinate second segment
+    :param z2: z height value seconc coordinate
+    :return: 3D distance between two input 3D coordinates
+    """
     d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
     return d
 
 
 def readin_json(jsonfile):
+    """
+    input: geojson or json file
+    """
     with open(jsonfile) as json_data:
         d = json.load(json_data)
         return d
 
 
-geoj_4326_file = "../geodata/velowire_stage_16_4326.geojson"
 geoj_27563_file = "../geodata/velowire_stage_16_27563.geojson"
 
+# create python dict type from geojson file object
 json_load = readin_json(geoj_27563_file)
 
-
-distance_values = []
+# set start lengths
 length_3d = 0.0
-length_2d = None
+length_2d = 0.0
+
+# go through each geometry in our linestring
 for f in json_load['features']:
+    # create shapely shape from geojson
     s = shape(f['geometry'])
     geo = f['geometry']
-    # print geo
-    length_2d = s.length
-    print s.coords[1]
-    print s.coords[2]
 
+    # calculate 2D total length
+    length_2d = s.length
+
+    # set start elevation
+    elevation_gain = 0
+
+    # go through each coordinate pair
     for vert_start, vert_end in pairs(s.coords):
         line_start = Point(vert_start)
         line_end = Point(vert_end)
 
+        # create
         x1 = line_start.coords[0][0]
-        x2 = line_end.coords[0][0]
         y1 = line_start.coords[0][1]
-        y2 = line_end.coords[0][1]
         z1 = line_start.coords[0][2]
+        x2 = line_end.coords[0][0]
+        y2 = line_end.coords[0][1]
         z2 = line_end.coords[0][2]
 
-        distance = calc_3d_distance_2pts(x1, y1, x2, y2, z1, z2)
+        distance = calc_3d_distance_2pts(x1, y1, z1, x2, y2, z2)
         length_3d += distance
 
+        # calculate total elevation gain
+        if z1 > z2:
+            elevation_gain = ((z1 - z2) + elevation_gain )
+            z2 = z1
+        else:
+            elevation_gain = elevation_gain # no height change
+            z2 = z1
+
+
+print "total elevation gain is: " + str(elevation_gain) + " meters"
 
 # print coord_pair
 print "3D line distance is: " + str(length_3d / 1000)
 print "2D line distance is: " + str(length_2d / 1000)
 print "difference 2D and 3D is: " + str(length_3d - length_2d) + " meters"
-
-# PostGIS 3D Distance and 2D Distance
-query = """"
-        SELECT ST_3DDistance(
-                    ST_GeomFromEWKT('SRID=3857;POINT(1 2 5)'),
-                    ST_GeomFromEWKT('SRID=3857;POINT(2 4 8)')
-                ) As dist_3d,
-                ST_Distance(
-                    ST_GeomFromEWKT('SRID=3857;POINT(1 2)'),
-                    ST_GeomFromEWKT('SRID=3857;POINT(2 4 8)')
-                ) As dist_2d;
-		"""
-query_gen_profile = """
-WITH points3d AS
-    (SELECT (ST_DumpPoints(geom)).geom AS geom,
-            ST_StartPoint(geom) AS origin
-     FROM geodata.velowire_stage_16_3d
-     WHERE id = id)
-SELECT ST_distance(origin, geom) AS x, ST_Z(geom) AS y,
-St_3ddistance(origin,geom)-st_distance(origin,geom) as diff_dist
---st_length(geom)
-FROM points3d;
-
-"""
