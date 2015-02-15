85a51e157911a13b8b7c20e6f76eb5f65aa2f217
mdiener21 <mdiener21@gmail.com>
2015-02-15 21:16:43 +0100

    proper 3d line length calculation vs. 2d line length

--- ch05/code/ch05-05_3d_line_distance.py
+++ ch05/code/ch05-05_3d_line_distance.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import math
-from shapely.geometry import shape
+from shapely.geometry import shape, Point
 import json
-import geojson
 
-# filename = '../geodata/velowire_stage_16-Carcassonne-Bagneres-de-Luchon.shp'
-filename = '../geodata/velowire_stage_16_4326.geojson'
-# filename = '../geodata/velowire_stage_16-Carcassonne-Bagneres-de-Luchon.kml'
+
 
 def pairs(lst):
     for i in range(1, len(lst)):
@@ -18,143 +15,49 @@ def calc_3d_distance_2pts(x1, y1, x2, y2, z1, z2):
     d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
     return d
 
-line_3d_distance = 0.0
 
 def readin_json(jsonfile):
     with open(jsonfile) as json_data:
         d = json.load(json_data)
         return d
 
-json_load = readin_json("../geodata/velowire_stage_16_27563.geojson")
-print type(json_load)
 
-# js = json.dumps(geojson_file.read().decode('utf-8'))
-# geojson_js = json.loads(geojson_file.read().decode('utf-8'))
-# jsx = json.dumps(geojson_js)
-# print jsx
-#
-# print geojson_js
-# print type(geojson_js)
+geoj_4326_file = "../geodata/velowire_stage_16_4326.geojson"
+geoj_27563_file = "../geodata/velowire_stage_16_27563.geojson"
+
+json_load = readin_json(geoj_27563_file)
 
 
+distance_values = []
+length_3d = 0.0
+length_2d = None
 for f in json_load['features']:
     s = shape(f['geometry'])
     geo = f['geometry']
-    #print geo
-    print s.length/1000
+    # print geo
+    length_2d = s.length
     print s.coords[1]
-    for pair in pairs(list(s.coords)):
-        x1 = pair[0][0]
-        x2 = pair[0][1]
-        z1 = pair[0][2]
-        y1 = pair[1][0]
-        y2 = pair[1][1]
-        z2 = pair[1][2]
+    print s.coords[2]
 
-        distance = calc_3d_distance_2pts(x1,y1,x2,y2,z1,z2)
-        #print distance
-        line_3d_distance += distance
+    for vert_start, vert_end in pairs(s.coords):
+        line_start = Point(vert_start)
+        line_end = Point(vert_end)
 
+        x1 = line_start.coords[0][0]
+        x2 = line_end.coords[0][0]
+        y1 = line_start.coords[0][1]
+        y2 = line_end.coords[0][1]
+        z1 = line_start.coords[0][2]
+        z2 = line_end.coords[0][2]
 
-print "3D line distance is: "  + str(line_3d_distance/1000000)
-
-from shapely.geometry import LineString
-# line_3d_distance = 0.0
-
-# with fiona.open(filename, 'r') as layer1:
-#     for feat1 in layer1:
-#         fid = int(feat1['id'])
-#         geom1 = shape(feat1['geometry'])
-#         print geom1.length/1000
-#         print geom1.coords[1]
-#         for pair in pairs(list(geom1.coords)):
-#             x1 = pair[0][0]
-#             x2 = pair[0][1]
-#             z1 = pair[0][2]
-#             y1 = pair[1][0]
-#             y2 = pair[1][1]
-#             z2 = pair[1][2]
-#
-#             distance = calc_3d_distance_2pts(x1,y1,x2,y2,z1,z2)
-#             #print distance
-#             line_3d_distance += distance
-# print "3D line distance is: "
-# print line_3d_distance/1000
-        # index.insert(fid, geom1.bounds)
-    # with fiona.open(intSHP, 'w', 'ESRI Shapefile', schema) as layer3:
-    #     layer3.write({ 'properties': props,
-    #                     'geometry': mapping(geom1.intersection(geom2))
-    #                  })
+        distance = calc_3d_distance_2pts(x1, y1, x2, y2, z1, z2)
+        length_3d += distance
 
 
-#distance_3d = calc_3d_distance_2pts(x1, y1, z1, x2, y2, z2)
-#print distance_3d
-
-# http://gis.stackexchange.com/questions/84512/get-the-vertices-on-a-linestring-either-side-of-a-point
-
-
-# import fiona
-#
-# with open('../geodata/bike_route.shp'):
-# line = LineString([(0, 0, 0), (1, 2, 0), (2, 2, 0), (2, 3, 0), (4, 2, 0), (5, 5, 0)])
-line = LineString([(1,2,5),(2,4,8)])
-#list(line.coords)
-
-#print line.length
-
-new_dist = 0.0
-for pair in pairs(list(line.coords)):
-    print pair
-    x1 = pair[0][0]
-    x2 = pair[0][1]
-    z1 = pair[0][2]
-    y1 = pair[1][0]
-    y2 = pair[1][1]
-    z2 = pair[1][2]
-
-    distance = calc_3d_distance_2pts(x1,y1,x2,y2,z1,z2)
-    #print distance
-    new_dist += distance
-    #new_dist = new_dist + distance
-    #print new_dist
-
-print new_dist
-
-print calc_3d_distance_2pts(1,2,2,4,5,8)
-print line.length
-
-def getSum(l):
-    sum = 0
-    skip = False
-    for i in l:
-         if i == 13:
-             skip = True
-             continue
-         if skip:
-             skip = False
-             continue
-         sum += i
-    return sum
-
-foo = [1, 13, 13, 2, 3]
-#print "foo"
-#print getSum(foo)
-#print "x"
-
-# num_vertex_in_line = len(line)
-# distance = 0
-# for vertex in line_geom:
-#
-#
-# # on first pass value should be zero
-#     # on second pass value should be zero + distance
-#     dist_3d = calc_3d_distance_2pts(v1, v_next)
-#
-#     x = x + 1
-#     # added each distance up
-#
-#     distance += dist_3d
-
+# print coord_pair
+print "3D line distance is: " + str(length_3d / 1000)
+print "2D line distance is: " + str(length_2d / 1000)
+print "difference 2D and 3D is: " + str(length_3d - length_2d) + " meters"
 
 # PostGIS 3D Distance and 2D Distance
 query = """"
@@ -167,3 +70,15 @@ query = """"
                     ST_GeomFromEWKT('SRID=3857;POINT(2 4 8)')
                 ) As dist_2d;
 		"""
+query_gen_profile = """
+WITH points3d AS
+    (SELECT (ST_DumpPoints(geom)).geom AS geom,
+            ST_StartPoint(geom) AS origin
+     FROM geodata.velowire_stage_16_3d
+     WHERE id = id)
+SELECT ST_distance(origin, geom) AS x, ST_Z(geom) AS y,
+St_3ddistance(origin,geom)-st_distance(origin,geom) as diff_dist
+--st_length(geom)
+FROM points3d;
+
+"""
