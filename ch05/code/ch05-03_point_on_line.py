c1257bf0ad2e197cfafad220be7c70c36878781e
mdiener21 <mdiener21@gmail.com>
2015-02-16 00:03:49 +0100

    added comments, re-arrange

--- ch05/code/ch05-03_point_on_line.py
+++ ch05/code/ch05-03_point_on_line.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from shapely.geometry import LineString, Point
+from shapely.geometry import LineString
 
+# define our simple line
 line = LineString([(0.5, 0.5), (2.0, 1.5), (3.0, 0.5)])
 
+# get the coordinates of each vertex in our line
 line_original = list(line.coords)
 print line_original
 
+# showing how to reverse a linestring
 line_reversed = list(line.coords)[::-1]
 print line_reversed
 
-point_on_line = line.interpolate(1.4)
-print point_on_line
-
+# example of the same reversing function on a string for example
 hello = 'hello world'
 reverse_hello = hello[::-1]
 print reverse_hello
+
+# locating the point on a line based on distance from line start
+point_on_line = line.interpolate(1.4)
+print point_on_line
