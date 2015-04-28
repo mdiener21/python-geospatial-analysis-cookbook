1b39abe68c6c1a00e099f10f024e66e0bf07dc13
mdiener21 <mdiener21@gmail.com>
2015-04-29 01:59:36 +0200

    ch07-05

---
+++ ch07/code/ch07-05_color_relief.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+
+dem_file = '../geodata/092j02_0200_demw.dem'
+hillshade_relief = '../geodata/hillshade.tif'
+relief = '../geodata/relief.tif'
+final_color_relief = '../geodata/final_color_relief.tif'
+
+create_hillshade = 'gdaldem hillshade -co compress=lzw -compute_edges ' + dem_file +  ' ' + hillshade_relief
+subprocess.call(create_hillshade)
+print create_hillshade
+
+cr = 'gdaldem color-relief -co compress=lzw ' + dem_file + ' ramp.txt ' + relief
+subprocess.call(cr)
+print cr
+
+merge = 'python hsv_merge.py ' + relief + ' ' + hillshade_relief + ' ' + final_color_relief
+subprocess.call(merge)
+print merge
+
+create_slope = '''gdaldem slope -co compress=lzw ../geodata/092j02_0200_demw.dem ../geodata/slope_w-degrees.tif '''
+
+subprocess.call(create_slope)
+
+
+# gdaldem hillshade -co compress=lzw -compute_edges -az 315 -alt 60 -z 2 in_relief.asc 315.tif
+# gdaldem hillshade -co compress=lzw -compute_edges -az 275 -alt 60 -z 2 in_relief.asc 275.tif
+# gdaldem hillshade -co compress=lzw -compute_edges -az 355 -alt 60 -z 2 in_relief.asc 355.tif
+# gdaldem hillshade -co compress=lzw -compute_edges -az 135 -alt 60 -z 2 in_relief.asc 135.tif
+# gdaldem slope -co compress=lzw in_relief.asc slope.tif
+# gdaldem color-relief -co compress=lzw slope.tif rampslope.txt slope_col.tif
+# gdaldem color-relief -co compress=lzw in_relief.asc ramp.txt relief_col.tif
