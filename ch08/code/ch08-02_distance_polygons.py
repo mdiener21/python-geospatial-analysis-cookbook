848f82a1d6a6ef0be85d7ab825ff90238be678e8
mdiener21 <mdiener21@gmail.com>
2015-05-12 23:36:30 +0200

    refactoring

--- ch08/code/ch08-02_distance_polygons.py
+++ ch08/code/ch08-02_distance_polygons.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import psycopg2
+import shapefile
 import json
+import shapely.geometry as geometry
 from geojson import loads, Feature, FeatureCollection
+from shapely.geometry import asShape
 
+# database connection
 db_host = "localhost"
 db_user = "postgres"
-db_passwd = "air" # secret
+db_passwd = "air"
 db_database = "py_geoan_cb"
 db_port = "5432"
 
@@ -16,92 +20,114 @@ conn = psycopg2.connect(host=db_host, user=db_user, port=db_port,
                         password=db_passwd, database=db_database)
 cur = conn.cursor()
 
+def write_geojson(outfilename, indata):
+    file_out = open(outfilename, "w")
+    file_out.write(json.dumps(indata))
 
+# center point for creating our distance polygons
 x_start_coord = 71384.9532168
 y_start_coord = 164571.903749
 
+# query including two variables for the x, y POINT coordinate
+start_node_query = """
+    SELECT id
+    FROM geodata.ch08_e01_networklines_vertices_pgr AS p
+    WHERE ST_DWithin(the_geom,
+             ST_GeomFromText('POINT(%s %s)',31255),1);"""
+
+# get the start node id as an integer
+# pass the variables
+cur.execute(start_node_query, (x_start_coord, y_start_coord))
+sn = int(cur.fetchone()[0])
+
+comb_res = []
+
+hallways = shapefile.Reader("../geodata/shp/e01_hallways_union.shp")
+e01_hallway_features = hallways.shape()
+e01_hallway_shply = asShape(e01_hallway_features)
+
+# time in seconds
 evac_times = [10, 20, 30, 60]
-distance_from_start = 60
+
+
 for evac_time in evac_times:
-    # query including two variables for the x, y POINT coordinate
-    start_node_query = """
-        SELECT id FROM geodata.ch08_e01_networklines_vertices_pgr AS p
-        WHERE ST_DWithin(the_geom,
-                 ST_GeomFromText('POINT(%s %s)',31255),1);"""
-
-    # get the start node id as an integer
-    # pass the variables
-    cur.execute(start_node_query, (x_start_coord, y_start_coord))
-    sn = int(cur.fetchone()[0])
-
-    print sn
 
     distance_poly_query = '''
         SELECT seq, id1 AS node, cost, ST_AsGeoJSON(the_geom)
             FROM pgr_drivingDistance(
-                    'SELECT ogc_fid as id, source, target, ST_Length(wkb_geometry)/5000*60*60 as cost FROM geodata.ch08_e01_networklines',
-                    {start_point}, {distance}, false, false
+                    'SELECT ogc_fid AS id, source, target,
+                        ST_Length(wkb_geometry)/5000*60*60 AS cost
+                     FROM geodata.ch08_e01_networklines',
+                    %s, %s, false, false
             ) as drive_dist
-            JOIN geodata.ch08_e01_networklines_vertices_pgr as networklines
+            JOIN geodata.ch08_e01_networklines_vertices_pgr
+            AS networklines
             ON drive_dist.id1 = networklines.id;
-        '''.format(start_point=sn, distance=evac_time)
-
-    test_poly_query = '''
-        SELECT seq, id1 AS node, cost
-            FROM pgr_drivingDistance(
-                    'SELECT ogc_fid as id, source, target, st_length(wkb_geometry)/5000*60*60 AS cost FROM geodata.ch08_e01_networklines',
-                    472, 20, false, false
-            );
         '''
 
-    # some_view = """
-    #  SELECT seq, id1 AS node, id2 AS edge, cost, geom
-    #   FROM pgr_drivingdistance(
-    #     'SELECT ogc_fid as id, source, target, traveltime_min as cost FROM network.publictransport',
-    #     1, 100000, false, false
-    #   ) as di
-    #   JOIN network.publictransport_nodes pt
-    #   ON di.id1 = pt.id;
-    #   """
-
-    cur.execute(distance_poly_query)
+    cur.execute(distance_poly_query,(sn, evac_time))
     # get entire query results to work with
     route_segments = cur.fetchall()
 
     # empty list to hold each segment for our GeoJSON output
-    route_result = []
+    route_results = []
 
     # loop over each segment in the result route segments
     # create the list of our new GeoJSON
     for segment in route_segments:
-        print segment
-        sequence = segment[0]
-        node = segment[1]
-        cost = segment[2]
+        sequence = segment[0]     # sequence number
+        node = segment[1]         # node id
+        cost = segment[2]         # cost value
+        geojs = segment[3]        # geometry
+        geojs_geom = loads(geojs) # create geojson geom
+        geojs_feat = Feature(geometry=geojs_geom,
+                properties={'sequence_num': sequence,
+                'node':node, 'evac_time_sec':cost,
+                'evac_code': evac_time})
+        # add each point to total including all points
+        comb_res.append(geojs_feat)
+        # add each point for individual evacuation time
+        route_results.append(geojs_geom)
 
-        geojs = segment[3]
-        geojs_geom = loads(geojs)
-        geojs_feat = Feature(geometry=geojs_geom, properties={'sequence_num': sequence,
-                                                              'node':node, 'evac_time_sec':cost,
-                                                              'evac_code': evac_time})
-        route_result.append(geojs_feat)
+    # geojson module creates GeoJSON Feature Collection
+    geojs_fc = FeatureCollection(route_results)
 
-    # using the geojson module to create our GeoJSON Feature Collection
-    geojs_fc = FeatureCollection(route_result)
+    # create list of points for each evac time
+    evac_time_pts = [asShape(route_segment) for route_segment in route_results]
 
-    # define the output folder and GeoJSON file name
-    output_geojson_route = "../geodata/ch08-02_dist_pts_60.geojson"
+    # code here is same as list comprehension above for better readability
+    # evac_time_pts = []
+    # for route_segment in route_results:
+    #     shply_geom = asShape(route_segment)
+    #     evac_time_pts.append(shply_geom)
 
-    output_geojson_route2 = "../geodata/ch08-02_dist_pts_" + str(evac_time) + ".geojson"
+    # create MultiPoint from our list of points for evac time
+    point_collection = geometry.MultiPoint(list(evac_time_pts))
 
-    # save geojson to a file in our geodata folder
-    def write_geojson():
-        file_out = open(output_geojson_route2, "w")
-        file_out.write(json.dumps(geojs_fc))
-        file_out.close()
+    #create our convex hull polyon around evac time points
+    convex_hull_polygon = point_collection.convex_hull
 
-    # run the write function to actually create the GeoJSON file
-    write_geojson()
+    # intersect convex hull with hallways polygon  (ch = convex hull)
+    ch_intersect = e01_hallway_shply.intersection(convex_hull_polygon)
+
+    # export convex hull intersection to geojson
+    ch = ch_intersect.__geo_interface__
+
+    # for each evac time we create a unique GeoJSON polygon
+    output_ply = "../geodata/ch08-02_dist_poly_" + str(evac_time) + ".geojson"
+
+    write_geojson(output_ply, ch)
+
+    output_geojson_route = "../geodata/ch08-02_dist_pts_" + str(evac_time) + ".geojson"
+
+    # save GeoJSON to a file in our geodata folder
+    write_geojson(output_geojson_route, geojs_fc )
+
+# final result GeoJSON
+final_res = FeatureCollection(comb_res)
+
+# write to disk
+write_geojson("../geodata/final_res.geojson", final_res)
 
 # clean up and close database curson and connection
 cur.close()
