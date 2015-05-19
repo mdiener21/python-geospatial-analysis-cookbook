b059b1b659db285ba91ddeb89a9fdab50d05f6a2
Michael Diener <mdiener21@gmail.com>
2015-05-19 11:23:38 +0200

    3d indoor route calculations

--- ch08/code/ch08-05_indoor_3d_route.py
+++ ch08/code/ch08-05_indoor_3d_route.py
@@ -7,7 +7,7 @@ from geojson import loads, Feature, FeatureCollection
 
 db_host = "localhost"
 db_user = "postgres"
-db_passwd = "secret"
+db_passwd = "air"
 db_database = "py_geoan_cb"
 db_port = "5432"
 
@@ -18,39 +18,53 @@ conn = psycopg2.connect(host=db_host, user=db_user, port=db_port,
 # create a cursor
 cur = conn.cursor()
 
-# start_coord = "71384.9532168 164571.903749"
-# end_coord = "71398.8429459 164493.503944"
+# define our start and end coordinates in EPSG:3857
+# set start and end floor level as integer 0,1,2 for example
+x_start_coord = 1587848.414
+y_start_coord = 5879564.080
+start_floor = 2
+
+x_end_coord = 1588005.547
+y_end_coord = 5879736.039
+end_floor = 2
+
 
 # find the start node id within 1 meter of the given coordinate
+# select from correct floor level using 3D Z value
+# our Z Value is the same as the floor number as an integer
 # used as input in routing query start point
 start_node_query = """
     SELECT id FROM geodata.networklines_3857_vertices_pgr AS p
-    WHERE ST_DWithin(the_geom, ST_GeomFromText('POINT(71384.9532168 164571.903749)',31255), 1);"""
+    WHERE ST_DWithin(the_geom, ST_GeomFromText('POINT(%s %s)',3857), 1)
+    AND ST_Z(the_geom) = %s;"""
 
 # locate the end node id within 1 meter of the given coordinate
 end_node_query = """
     SELECT id FROM geodata.networklines_3857_vertices_pgr AS p
-    WHERE ST_DWithin(the_geom, ST_GeomFromText('POINT(71398.8429459 164493.503944)',31255), 1);
-    """
+    WHERE ST_DWithin(the_geom, ST_GeomFromText('POINT(%s %s)',3857), 1)
+    AND ST_Z(the_geom) = %s;"""
 
-# get the start node id as an integer
-cur.execute(start_node_query)
-sn = int(cur.fetchone()[0])
+# run our query and pass in the 3 variables to the query
+# make sure the order of variables is the same as the
+# order in your query
+cur.execute(start_node_query, (x_start_coord, y_start_coord, start_floor))
+start_node_id = int(cur.fetchone()[0])
 
 # get the end node id as an integer
-cur.execute(end_node_query)
-en = int(cur.fetchone()[0])
+cur.execute(end_node_query, (x_end_coord, y_end_coord, end_floor))
+end_node_id = int(cur.fetchone()[0])
 
 
 # pgRouting query to return our list of segments representing
 # our shortest path Dijkstra results as GeoJSON
-# query returns the shortes path between our start and end nodes above
-# using the pytohn .format string syntax to insert a variable in the query
+# query returns the shortest path between our start and end nodes above
+# in 3D traversing floor levels and passing in the layer value = floor
+
 routing_query = '''
-    SELECT seq, id1 AS node, id2 AS edge, ST_Length(wkb_geometry) AS cost,
+    SELECT seq, id1 AS node, id2 AS edge, ST_Length(wkb_geometry) AS cost, layer,
            ST_AsGeoJSON(wkb_geometry) AS geoj
       FROM pgr_dijkstra(
-        'SELECT ogc_fid as id, source, target, st_length(wkb_geometry) as cost
+        'SELECT ogc_fid as id, source, target, st_length(wkb_geometry) AS cost, layer
          FROM geodata.networklines_3857',
         %s, %s, FALSE, FALSE
       ) AS dij_route
@@ -60,7 +74,7 @@ routing_query = '''
 
 
 # run our shortest path query
-cur.execute(routing_query, (sn, en))
+cur.execute(routing_query, (start_node_id, end_node_id))
 
 # get entire query results to work with
 route_segments = cur.fetchall()
@@ -72,9 +86,11 @@ route_result = []
 # create the list of our new GeoJSON
 for segment in route_segments:
     print segment
-    geojs = segment[4]
-    geojs_geom = loads(geojs)
-    geojs_feat = Feature(geometry=geojs_geom, properties={'nice': 'route'})
+    seg_cost = segment[3]     # cost value
+    layer_level = segment[4]  # floor number
+    geojs = segment[5]        # geojson coordinates
+    geojs_geom = loads(geojs) # load string to geom
+    geojs_feat = Feature(geometry=geojs_geom, properties={'floor': layer_level, 'cost': seg_cost})
     route_result.append(geojs_feat)
 
 # using the geojson module to create our GeoJSON Feature Collection
