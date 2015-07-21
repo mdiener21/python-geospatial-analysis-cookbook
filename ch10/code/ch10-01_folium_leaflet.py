db6f523684f6b6c4b6cd6be4941ea469589c0b54
Michael Diener <mdiener21@gmail.com>
2015-07-21 11:03:01 +0200

    fix paths

--- ch10/code/ch10-01_folium_leaflet.py
+++ ch10/code/ch10-01_folium_leaflet.py
@@ -3,12 +3,19 @@
 import folium
 import pandas as pd
 
+# define the polygons
 states_geojson = r'us-states.json'
-state_unemployment = r'html/US_Unemployment_Oct2012.csv'
+
+# statistic data to connect to our polygons
+state_unemployment = r'../www/html/US_Unemployment_Oct2012.csv'
+
+# read the csv statistic data
 state_data = pd.read_csv(state_unemployment)
 
-#Let Folium determine the scale
+# Let Folium determine the scale
 map = folium.Map(location=[48, -102], zoom_start=3, tiles="Stamen Toner")
+
+# create the leaflet map settings
 map.geo_json(geo_path=states_geojson, data=state_data,
              columns=['State', 'Unemployment'],
              threshold_scale=[5, 6, 7, 8, 9, 10],
@@ -16,4 +23,5 @@ map.geo_json(geo_path=states_geojson, data=state_data,
              fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2,
              legend_name='Unemployment Rate (%)')
 
-map.create_map(path='html/us_states.html')
+# output the final map file
+map.create_map(path='../www/html/ch10-01_folium_map.html')
