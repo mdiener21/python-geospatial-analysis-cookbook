26d799a5e8866b8026896d85e616de0c776a3843
mdiener21 <mdiener21@gmail.com>
2015-07-28 23:38:42 +0200

    added logger, error checking and more comments

--- ch11/code/web_analysis/api/views.py
+++ ch11/code/web_analysis/api/views.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 from django.db import connection
 
 
-
 def find_closest_network_node(x_coord, y_coord, floor):
     """
     Enter a given coordinate x,y and floor number and
@@ -23,24 +22,35 @@ def find_closest_network_node(x_coord, y_coord, floor):
     :return: node id as an integer
     """
     # connect to our Database
+    logger.debug("now running function find_closest_network_node")
     cur = connection.cursor()
 
+    # find nearest node on network within 200 m
+    # and snap to nearest node
     query = """ SELECT
         verts.id as id
         FROM geodata.networklines_3857_vertices_pgr AS verts
         INNER JOIN
           (select ST_PointFromText('POINT(%s %s %s)', 3857)as geom) AS pt
-        ON ST_DWithin(verts.the_geom, pt.geom, 10.0)
+        ON ST_DWithin(verts.the_geom, pt.geom, 200.0)
         ORDER BY ST_3DDistance(verts.the_geom, pt.geom)
         LIMIT 1;"""
 
     # pass 3 variables to our %s %s %s place holder in query
     cur.execute(query, (x_coord, y_coord, floor,))
 
-    # convert our result to an integer of the node id
-    point_on_networkline = int(cur.fetchone()[0])
+    # get the result
+    query_result = cur.fetchone()
 
-    return point_on_networkline
+    # check if result is not empty
+    if query_result is not None:
+        # get first result in tuple response there is only one
+        point_on_networkline = int(query_result[0])
+        return point_on_networkline
+    else:
+        logger.debug("query is none check tolerance value of 200")
+        return False
+
 
 # use the rest_framework decorator to create our api
 #  view for get, post requests
@@ -58,12 +68,12 @@ def create_route(request, start_coord, start_floor, end_coord, end_floor):
     :return: GeoJSON route
     """
 
-
     if request.method == 'GET' or request.method == 'POST':
 
         cur = connection.cursor()
 
-        # parse the incoming coordinates and floor using split
+        # parse the incoming coordinates and floor using
+        # split by comma
         x_start_coord = float(start_coord.split(',')[0])
         y_start_coord = float(start_coord.split(',')[1])
         start_floor_num = int(start_floor)
@@ -97,9 +107,14 @@ def create_route(request, start_coord, start_floor, end_coord, end_floor):
               ON dij_route.id2 = input_network.ogc_fid ;
           '''
 
-
         # run our shortest path query
-        cur.execute(routing_query, (start_node_id, end_node_id))
+        if start_node_id or end_node_id:
+            cur.execute(routing_query, (start_node_id, end_node_id))
+        else:
+            logger.error("start or end node is None "
+                         + str(start_node_id))
+            return HttpResponseNotFound('<h1>Sorry NO start or end node'
+                                        ' found within 200m</h1>')
 
         # get entire query results to work with
         route_segments = cur.fetchall()
@@ -110,11 +125,11 @@ def create_route(request, start_coord, start_floor, end_coord, end_floor):
         # loop over each segment in the result route segments
         # create the list of our new GeoJSON
         for segment in route_segments:
-            seg_cost = segment[3]     # cost value
-            layer_level = segment[4]  # floor number
+            seg_cost = segment[3]      # cost value
+            layer_level = segment[4]   # floor number
             seg_type = segment[5]
-            geojs = segment[6]        # geojson coordinates
-            geojs_geom = loads(geojs) # load string to geom
+            geojs = segment[6]         # geojson coordinates
+            geojs_geom = loads(geojs)  # load string to geom
             geojs_feat = Feature(geometry=geojs_geom,
                                  properties={'floor': layer_level,
                                               'length': seg_cost,
