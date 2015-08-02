89a5b1248ba6be38df53458a1cb00ef2a8e11f77
mdiener21 <mdiener21@gmail.com>
2015-08-02 13:04:24 +0200

    added parameter for route types

--- ch11/code/web_analysis/api/urls.py
+++ ch11/code/web_analysis/api/urls.py
@@ -3,8 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('api.views',
     #  ex valid call from to  /api/directions/1587848.414,5879564.080,2&1588005.547,5879736.039,2
-    url(r'^directions/(?P<start_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<start_floor>\d+)&(?P<end_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<end_floor>\d+)/$', 'create_route', name='directions'),
-
+    url(r'^directions/(?P<start_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<start_floor>\d+)&(?P<end_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<end_floor>\d+)&(?P<route_type>[0-9])/$', 'create_route', name='directions'),
 
 )
 
