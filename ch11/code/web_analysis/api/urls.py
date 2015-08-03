c8ec2ba1b44ba84b2fbf5e4fcf295b03ad548f81
mdiener21 <mdiener21@gmail.com>
2015-08-03 21:11:08 +0200

    added new urls room to room  and rooms

--- ch11/code/web_analysis/api/urls.py
+++ ch11/code/web_analysis/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns('api.views',
     #  ex valid call from to  /api/directions/1587848.414,5879564.080,2&1588005.547,5879736.039,2
     url(r'^directions/(?P<start_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<start_floor>\d+)&(?P<end_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<end_floor>\d+)&(?P<route_type>[0-9])/$', 'create_route', name='directions'),
+    url(r'^directions/(?P<start_room_num>\d{5})&(?P<end_room_num>\d{5})&(?P<route_type>[0-9])/$', 'route_room_to_room', name='route-room-to-room'),
+    url(r'^directions/(?P<room_num>\d{5})/$', 'get_room_centroid_node', name='room-center'),
+    url(r'^rooms/$', 'room_list', name='room-list'),
 
 )
 
