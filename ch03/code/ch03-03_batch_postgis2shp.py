6d08199244b75bc809f1666080081ad41d563a34
Michael Diener <mdiener21@gmail.com>
2015-08-13 23:52:29 +0200

    refactor, error check if dir exists

--- ch03/code/ch03-03_batch_postgis2shp.py
+++ ch03/code/ch03-03_batch_postgis2shp.py
@@ -5,28 +5,28 @@ import subprocess
 import os
 
 # folder to hold output Shapefiles
-destination_dir = os.path.dirname(os.path.realpath('..\geodata')) + "\\geodata\\temp"
+destination_dir = os.path.realpath('../geodata/temp')
 
 # list of postGIS tables
-postgis_tables_list = ["bikeways", "sportanl"]
+postgis_tables_list = ["bikeways", "highest_mountains"]
 
 # database connection parameters
-db_schema = "active_schema=geodata"
-db_connection = "PG:host=localhost port=5432 user=postgres \
-        dbname=py_geoan_cb password=air " + db_schema
+db_connection = """PG:host=localhost port=5432 user=pluto
+        dbname=py_geoan_cb password=stars active_schema=geodata"""
 
 output_format = "ESRI Shapefile"
 
 # check if destination directory exists
-if os.path.isdir(destination_dir):
+if not os.path.isdir(destination_dir):
+    os.mkdir(destination_dir)
     for table in postgis_tables_list:
         subprocess.call(["ogr2ogr", "-f", output_format, destination_dir,
                          db_connection, table])
         print("running ogr2ogr on table: " + table)
 else:
-    print("oh no your destination directory" + destination_dir + "does not exist please create it then run again")
+    print("oh no your destination directory " + destination_dir +
+          " already exist please remove it then run again")
 
 # commandline call without using python will look like this
 # ogr2ogr -f "ESRI Shapefile" mydatadump \
 # PG:"host=myhost user=myloginname dbname=mydbname password=mypassword" neighborhood parcels
-
