4bf121b642a061f64a9b0406868e9051dafeb9c1
Michael Diener <mdiener21@gmail.com>
2015-08-13 22:19:50 +0200

    refactor

--- ch03/code/ch03-01-shp2pg.py
+++ ch03/code/ch03-01-shp2pg.py
@@ -10,14 +10,14 @@ geom_type = "MULTILINESTRING"
 output_format = "PostgreSQL"
 
 # database connection string
-db_connection = "PG:host=localhost port=5432 \
-	user=postgres dbname=py_geoan_cb password=secret"
+db_connection = """PG:host=localhost port=5432
+	user=pluto dbname=py_test password=stars"""
 
 # input shapefile
 input_shp = "../geodata/bikeways.shp"
 
 # call ogr2ogr from python
-subprocess.call(["ogr2ogr","-lco", db_schema, "-lco", overwrite_option, \
+subprocess.call(["ogr2ogr","-lco", db_schema, "-lco", overwrite_option,
 	"-nlt", geom_type, "-f", output_format, db_connection,  input_shp])
 	
 # -lco is the layer creation option
