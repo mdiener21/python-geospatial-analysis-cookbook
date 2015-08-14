427552ee262aca1ee5492d77a4cfa579ccd90f97
Michael Diener <mdiener21@gmail.com>
2015-08-14 10:06:26 +0200

    fixed osm to shapefile converter

--- ch03/code/ch03-04_osm2shp.py
+++ ch03/code/ch03-04_osm2shp.py
@@ -3,15 +3,14 @@
 
 # convert / import osm xml .osm file into a Shapefile
 import subprocess
+import os
+import shutil
 
 # specify output format
 output_format = "ESRI Shapefile"
 
 # complete path to input OSM xml file .osm
 input_osm = '../geodata/OSM_san_francisco_westbluff.osm'
-
-# path to a new folder to hold the Shapefiles (must not exist)
-output_shp_folder = '../geodata/shp'
 
 # Windows users can uncomment these two lines if needed
 # ogr2ogr = r"c:/OSGeo4W/bin/ogr2ogr.exe"
@@ -20,5 +19,32 @@ output_shp_folder = '../geodata/shp'
 # view what geometry types are available in our OSM file
 subprocess.call([ogr_info, input_osm])
 
-# call the subprocess function which fires ogr2ogr command line
-subprocess.call([ogr2ogr, "-f", output_format, output_shp_folder, input_osm])
+destination_dir = os.path.realpath('../geodata/temp')
+
+if os.path.isdir(destination_dir):
+    # remove output folder if it exists
+    shutil.rmtree(destination_dir)
+    print("removing existing directory : " + destination_dir)
+    # create new output folder
+    os.mkdir(destination_dir)
+    print("creating new directory : " + destination_dir)
+
+    # list of geometry types to convert to Shapefile
+    geom_types = ["lines", "points", "multilinestrings", "multipolygons"]
+
+    # create a new Shapefile for each geometry type
+    for g_type in geom_types:
+
+        subprocess.call([ogr2ogr,
+               "-skipfailures", "-f", output_format,
+                 destination_dir, input_osm,
+                 "layer", g_type,
+                 "--config","OSM_USE_CUSTOM_INDEXING", "NO"])
+        print("done creating " + g_type)
+
+# if you like to export to SPATIALITE from .osm
+# subprocess.call([ogr2ogr, "-skipfailures", "-f",
+#         "SQLITE", "-dsco", "SPATIALITE=YES",
+#         "my2.sqlite", input_osm])
+
+
