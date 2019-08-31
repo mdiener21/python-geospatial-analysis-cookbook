4063de9592f799f7d492d5e0c3ed9e4aa246394d
Michael Diener <mdiener21@users.noreply.github.com>
2019-08-31 14:38:07 +0200

    remove unused import

--- ch04/code/ch04-03_postgis_line_intersections.py
+++ ch04/code/ch04-03_postgis_line_intersections.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import psycopg2
-import json
-from geojson import loads, Feature, FeatureCollection
-
 
 # Database Connection Info
 db_host = "localhost"
